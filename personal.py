7e19579eb82533a989f9953ef731f7d15de83ccb
Maksym-Min <4906595@gmail.com>
2023-05-03 16:14:27 +0200

    initial

---
+++ personal.py
@@ -0,0 +1,10 @@
+
+
+personal = {
+    'DBNAME': 'project_sql',
+    'USER': 'skillfactory',
+    'HOST': '84.201.134.129',
+    'PASSWORD': 'cCkxxLVrDE8EbvjueeMedPKt' ,
+    'PORT':5432
+        
+}
